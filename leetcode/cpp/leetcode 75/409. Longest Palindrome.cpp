cabd2607900e1b51357d98cdfde3982726655108
sojebsikder <sojebsikder@gmail.com>
2023-02-26 11:33:07 +0600

    main

--- leetcode/cpp/leetcode 75/409. Longest Palindrome.cpp
+++ leetcode/cpp/leetcode 75/409. Longest Palindrome.cpp
@@ -7,17 +7,38 @@ class Solution
 public:
     int longestPalindrome(string s)
     {
+        int oddCount = 0;
+        unordered_map<char, int> ump;
+
+        for (char ch : s)
+        {
+            ump[ch]++;
+            if (ump[ch] % 2 == 1)
+            {
+                oddCount++;
+            }
+            else
+            {
+                oddCount--;
+            }
+        }
+        if (oddCount > 1)
+        {
+            return s.length() - oddCount + 1;
+        }
+        return s.length();
     }
 };
+
 int main()
 {
-    string s;
+    string s = "abccccdd";
 
     int result;
 
     result = (new Solution())->longestPalindrome(s);
 
-    cout << result << endl;
+    cout << result << endl; // 7
 
     return 0;
 }
