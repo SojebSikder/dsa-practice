b3cf6c8c7d4f584cc2f5f14f81e8cd86eee1ea67
sojebsikder <sojebsikder@gmail.com>
2023-06-29 23:52:23 +0600

    main

---
+++ leetcode/cpp/232. Implement Queue using Stacks.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyQueue
+{
+public:
+    MyQueue()
+    {
+    }
+
+    void push(int x)
+    {
+    }
+
+    int pop()
+    {
+    }
+
+    int peek()
+    {
+    }
+
+    bool empty()
+    {
+    }
+};
+
+int main()
+{
+
+    MyQueue *obj = new MyQueue();
+    obj->push(10);
+    int param_2 = obj->pop();
+    int param_3 = obj->peek();
+    bool param_4 = obj->empty();
+
+    return 0;
+}
