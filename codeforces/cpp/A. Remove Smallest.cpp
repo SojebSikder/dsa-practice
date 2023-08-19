b484270adee7c140f4aa97155b4224cd61190a4d
sojebsikder <sojebsikder@gmail.com>
2023-08-19 16:35:30 +0600

    added problem

---
+++ codeforces/cpp/A. Remove Smallest.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (abs(a[i] - a[i + 1]) > 1)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
