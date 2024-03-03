31a4583a0585a773ead7f6ab52bfc4d8ad2c19f5
sojebsikder <sojebsikder@gmail.com>
2024-03-03 19:31:18 +0600

    Updated leetcode problem

--- leetcode/cpp/70. Climbing Stairs.cpp
+++ leetcode/cpp/70. Climbing Stairs.cpp
@@ -5,17 +5,29 @@ using namespace std;
 class Solution
 {
 public:
+    // int climbStairs(int n)
+    // {
+    //     int prev2 = 1, prev1 = 1; // initally at 0th and 1st index
+
+    //     for (int i = 2; i <= n; i++)
+    //     {
+    //         int curr = prev1 + prev2;
+    //         prev2 = prev1; // update pointers
+    //         prev1 = curr;
+    //     }
+    //     return prev1;
+    // }
     int climbStairs(int n)
     {
-        int prev2 = 1, prev1 = 1; // initally at 0th and 1st index
+        int one = 1, two = 1;
 
-        for (int i = 2; i <= n; i++)
+        for (int i = 1; i < n; i++)
         {
-            int curr = prev1 + prev2;
-            prev2 = prev1; // update pointers
-            prev1 = curr;
+            int temp = one;
+            one = one + two;
+            two = temp;
         }
-        return prev1;
+        return one;
     }
 };
 
