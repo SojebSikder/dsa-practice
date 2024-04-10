044e8217bff8a3a0e00e9b2141ec7a1186516432
sojebsikder <sojebsikder@gmail.com>
2024-04-10 15:37:44 +0600

    updated code

--- codeforces/cpp/B. Make Equal.cpp
+++ codeforces/cpp/B. Make Equal.cpp
@@ -8,23 +8,23 @@ void solve()
     cin >> n;
 
     vector<int> a(n);
-    vector<int> ans(n);
 
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        ans[a[i] - 1] = i + 1;
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << ans[i] << " ";
-    }
+    
 }
 
 int main()
 {
-    solve();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 
     return 0;
 }
