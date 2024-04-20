9f62d2a679a28eb9f99d7b838ecb2e353e787296
sojebsikder <sojebsikder@gmail.com>
2024-04-20 12:09:10 +0600

    added leetcode problem

---
+++ leetcode/cpp/200. Number of Islands.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[i].size(); j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    void dfs(vector<vector<char>> &grid, int i, int j)
+    {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || grid[i][j] == '0')
+        {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+};
+
+int main()
+{
+    vector<vector<char>> grid = {
+        {'1', '1', '1', '1', '0'},
+        {'1', '1', '0', '1', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '0', '0', '0'}};
+
+    int result = Solution().numIslands(grid);
+
+    cout << result << endl;
+
+    return 0;
+}
