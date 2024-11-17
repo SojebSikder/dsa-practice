cd1f74c0f3b4ffb8057ee44bca395881969e104b
sojebsikder <sojebsikder@gmail.com>
2024-11-17 12:17:40 +0600

    added leetcode problem

--- leetcode/cpp/143. Reorder List.cpp
+++ leetcode/cpp/143. Reorder List.cpp
@@ -16,7 +16,39 @@ class Solution
 public:
     void reorderList(ListNode *head)
     {
+        // find middle
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
 
+        // reverse second half
+        ListNode *second = slow->next;
+        ListNode *prev = NULL;
+        slow->next = NULL;
+        while (second)
+        {
+            ListNode *temp = second->next;
+            second->next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        // merge two halfs
+        ListNode *first = head;
+        second = prev;
+        while (second)
+        {
+            ListNode *temp1 = first->next;
+            ListNode *temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
     }
 };
 
@@ -35,7 +67,6 @@ int main()
         cout << list->val << endl; //[1,5,2,4,3]
         list = list->next;
     }
-
 
     return 0;
 }
