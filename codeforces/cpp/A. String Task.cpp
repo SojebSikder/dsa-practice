35582f3949f41a12b820ff4e800ca2f94d4b4a34
sojebsikder <sojebsikder@gmail.com>
2025-04-30 20:27:44 +0600

    added codeforces problem

---
+++ codeforces/cpp/A. String Task.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+void solve()
+{
+    string n;
+    cin >> n;
+
+    for (auto c : n)
+    {
+        char ch = (char)tolower(c);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        {
+            continue;
+        }
+        else
+        {
+            cout << '.' << (char)tolower(c);
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    solve();
+
+    return 0;
+}
